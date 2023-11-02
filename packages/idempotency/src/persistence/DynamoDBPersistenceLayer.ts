6c0ade0d718c6b2d9da8c7b2aa31009b4ee574bb
Theophilus Ajayi <tolutheo@gmail.com>
2023-11-02 11:00:55 +0100

    Update to fetch ConditionalCheckFailedException Item.

--- packages/idempotency/src/persistence/DynamoDBPersistenceLayer.ts
+++ packages/idempotency/src/persistence/DynamoDBPersistenceLayer.ts
@@ -6,6 +6,7 @@ import { IdempotencyRecordStatus } from '../constants';
 import type { DynamoDBPersistenceOptions } from '../types';
 import {
   AttributeValue,
+  ConditionalCheckFailedException,
   DeleteItemCommand,
   DynamoDBClient,
   DynamoDBClientConfig,
@@ -200,15 +201,13 @@ class DynamoDBPersistenceLayer extends BasePersistenceLayer {
       );
     } catch (error) {
       if (error instanceof DynamoDBServiceException) {
-        if (error.name === 'ConditionalCheckFailedException') {
-          throw new IdempotencyItemAlreadyExistsError(
-            `Failed to put record for already existing idempotency key: ${record.idempotencyKey}`
-          );
-        }
+          if (error instanceof ConditionalCheckFailedException) {
+            throw new IdempotencyItemAlreadyExistsError(
+              `Failed to put record for already existing idempotency key: ${record.idempotencyKey}`,
+              error.Item
+            );
+          }
       }
-
-      throw error;
-    }
   }
 
   protected async _updateRecord(record: IdempotencyRecord): Promise<void> {
